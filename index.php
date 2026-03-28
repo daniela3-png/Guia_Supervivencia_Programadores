a9675345ab74dd358ee57616bac5a8d3fb61ee0c
mango <d.montecinos.vasquez@gmail.com>
2026-03-28 20:08:51 -0300

    feat: base de la guia de supervivencia act2

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
-<html lang="es">
 <head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1.0">
+    <title>Guía de Supervivencia - Daniela</title>
+
     <style>
         body {
             background-color: #a625e8;
@@ -34,13 +37,21 @@
         th, td {
             border: 1px solid black;
             padding: 8px;
+            text-align: center; /* Aprovechamos de centrar el texto de la tabla */
+        }
+
+        /* Clase para centrar las imágenes como querías */
+        td img {
+            display: block;
+            margin: 0 auto;
+            border-radius: 8px; /* Un toque de la semana 3 */
         }
     </style>
 </head>
 <body>
 
 <header>
-    <h1> Guía de supervivencia para Programadores novatos...</h1>
+    <h1>Guía de supervivencia para Programadores novatos...</h1>
 </header>
 
 <nav>
@@ -50,27 +61,30 @@
 <main>
     <section>
         <h2>Bienvenidos</h2>
-        <p> Un nuevo espacio de experimentación en código, espero no morir en el intento.</p>
+        <p>Un nuevo espacio de experimentación en código, espero no morir en el intento.</p>
     </section>
 
     <section>
         <h2>Mis Aliados</h2>
         <table>
-            <thead> <tr>
+            <thead>
+            <tr>
                 <th>Foto</th>
                 <th>Nombre</th>
             </tr>
             </thead>
-            <tbody> <tr>
-                <td><img src="Hatsune.jpg" width="100"></td>
-                <td> Virgen Hatsune Miku</td>
+            <tbody>
+            <tr>
+                <td><img src="Hatsune.jpg" width="100" alt="Virgen Hatsune Miku"></td>
+                <td>Virgen Hatsune Miku</td>
             </tr>
             <tr>
-                <td><img src="jojojisus.webp" width="100"></td>
-                <td> JoJo Jisus</td>
+                <td><img src="jojojisus.webp" width="100" alt="JoJo Jisus"></td>
+                <td>JoJo Jisus</td>
             </tr>
             </tbody>
-            <tfoot> <tr>
+            <tfoot>
+            <tr>
                 <td colspan="2">Total de aliados: 2</td>
             </tr>
             </tfoot>
@@ -83,4 +97,3 @@
 </footer>
 
 </body>
-</html>
